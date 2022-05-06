cdeaeb4bcf56ff42d0b2ae1a8228aefcfc056292
horse <horsescary@gmail.com>
2022-05-06 14:55:24 -0400

    its basicly just hello world rn

---
+++ main.py
@@ -0,0 +1,22 @@
+import gi
+import sys
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class myWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Hello World")
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+    
+    def on_button_clicked(self, widget):
+        print("Hello World")
+
+win = myWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()

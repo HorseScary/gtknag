f99ed4bd390e187656e912228430c31a779fe8cb
horse <horsescary@gmail.com>
2022-05-13 08:55:21 -0400

    stuff idk

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import gi
-import sys
+from os import system
+from sys import argv
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+
 class myWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Hello World")
+        self.box = Gtk.Box(spacing=5)
+
+        for i in range(0,len(argv)):
+            if argv[i] == '-t' or argv[i] == '--title':
+                super().__init__(title=argv[i + 1])
+            
+            elif argv[i] == '-l' or argv[i] == '--label':
+                self.label = Gtk.Label(label=argv[i+1])
+                self.box.pack_start(self.label, True, True, 0)
+            
+            elif argv[i] == '-b' or argv[i] == '--button':
+                self.button = Gtk.Button()
+                self.button.connect("clicked", self.on_button_clicked)
+                self.box.pack_start(self.button, True, True, 0)
+        
+
+        self.label = Gtk.Label(label="Button", angle=25)
+        self.box.pack_start(self.label, True, True, 0)
 
         self.button = Gtk.Button(label="Click Here")
         self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
+        self.box.pack_start(self.button, True, True, 0)
+
+        self.add(self.box)
     
-    def on_button_clicked(self, widget):
-        print("Hello World")
+    def on_button_clicked(self, button):
+        pass
 
 win = myWindow()
 win.connect("destroy", Gtk.main_quit)
---
+++ parseArgs.py
@@ -0,0 +1,16 @@
+def parseArgs(args):
+    i = 0 
+    labelCount = 0
+    buttonCount = 0
+    for arg in args:
+        if arg == "-L" or arg == "--label":
+            labelCount += 1
+            label + 
+
+        ++i
+
+'''
+make dictionary
+when new thing is hit, make new entry with list of the things for that new gtk item
+iterate through dictionary and through the lists in the dictionary to make the items  
+'''

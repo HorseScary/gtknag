fd766e2a466f3a3d6596c0912424f84373e00cb9
horse <horsescary@gmail.com>
2022-05-20 11:51:19 -0400

    updated readme, added basic functonality

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 import gi
 from os import system
-from sys import argv
+import argparse
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+global parser
+parser = argparse.ArgumentParser(description="owo")
+parser.add_argument("-b", "--button", type=str, nargs=2, help="<label> <'command'>")
+parser.add_argument("-m", "--message", type=str, nargs=1, help="<message>")
+parser.parse_args()
+global args
+args = parser.parse_args()
 
 class myWindow(Gtk.Window):
     def __init__(self):
+        super().__init__(title="ljalkjflskj")
+
         self.box = Gtk.Box(spacing=5)
+        self.add(self.box)
 
-        for i in range(0,len(argv)):
-            if argv[i] == '-t' or argv[i] == '--title':
-                super().__init__(title=argv[i + 1])
-            
-            elif argv[i] == '-l' or argv[i] == '--label':
-                self.label = Gtk.Label(label=argv[i+1])
-                self.box.pack_start(self.label, True, True, 0)
-            
-            elif argv[i] == '-b' or argv[i] == '--button':
-                self.button = Gtk.Button()
-                self.button.connect("clicked", self.on_button_clicked)
-                self.box.pack_start(self.button, True, True, 0)
-        
-
-        self.label = Gtk.Label(label="Button", angle=25)
+        self.label = Gtk.Label(label=args.message[0])
         self.box.pack_start(self.label, True, True, 0)
-
-        self.button = Gtk.Button(label="Click Here")
+        
+        self.button = Gtk.Button(label=args.button[0])
         self.button.connect("clicked", self.on_button_clicked)
         self.box.pack_start(self.button, True, True, 0)
 
-        self.add(self.box)
     
     def on_button_clicked(self, button):
-        pass
+        system(args.button[1])
 
 win = myWindow()
 win.connect("destroy", Gtk.main_quit)
